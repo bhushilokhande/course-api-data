e349bfc6cecaee6c3ead739bee68898bd1f39147
javaiu <39777474+javaiu@users.noreply.github.com>
2018-06-01 15:58:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+	// put setup code here
+	createCanvas(400,400);
+  
+}
+
+function draw() {
+  // put drawing code here
+     background(255,0,0);
+    //console.log("Temp");
+    //console.log("Temp");
+    ellipse(50, 50, 80, 80);
+    rect(25,30,15,15);
+    rect(60,30,15,15);
+}  
